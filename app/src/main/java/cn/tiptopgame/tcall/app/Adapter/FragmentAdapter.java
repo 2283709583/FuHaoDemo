de3fc8be887cdb2014b540803c316dc9d9576654
FragmentAdapter <2283709583@qq.com>
2019-03-27 10:49:17 +0800

    new class

---
+++ app/src/main/java/cn/tiptopgame/tcall/app/Adapter/FragmentAdapter.java
@@ -0,0 +1,26 @@
+package cn.tiptopgame.tcall.app.Adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import java.util.ArrayList;
+
+//FragmentAdapter
+public class FragmentAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragmentlist;
+
+    public FragmentAdapter(FragmentManager fm, ArrayList<Fragment> fragmentlist) {
+        super(fm);
+        this.fragmentlist = fragmentlist;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentlist.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentlist.size();
+    }
+}
